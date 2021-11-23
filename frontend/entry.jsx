59d375b7e6d8317b2b3655d1dfebd19da5dff6fd
mattpettenato <mattpettenato@gmail.com>
2021-11-22 17:41:03 -0800

    heroku test

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ReactDOM.render(<Root store={store} />, root);
   ReactDOM.render(<h1> Hello </h1>, root);
 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 });
 
