c1bcf55942c694b4d1805cf69648cb717ad10af9
mattpettenato <mattpettenato@gmail.com>
2022-02-18 16:27:55 -0800

    test

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -34,6 +34,11 @@ class BusinessShow extends React.Component {
 
   render() {
     
+    var options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+    var prnDt = 'Printed on ' + new Date().toLocaleTimeString('en-us', options);
+
+console.log(prnDt);
+
     const { business } = this.props;
 
     if (this.props.currentBusiness !== undefined) {
