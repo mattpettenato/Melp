0fba912b3925e7befa6dd532d26b2c95b089b5db
mattpettenato <mattpettenato@gmail.com>
2022-04-18 16:19:09 -0700

    select list

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -80,7 +80,29 @@ class SearchIndex extends React.Component {
       <div className="left-sided">
         <div className="category-filter">
           <h2>Categories</h2>
-          <Link to={`/businesses/search/all`} replace>
+  <select name="search-term" id="terms">
+    <option value="all">All</option>
+    <option value="italian">Italian</option>
+    <option value="asian">Asian</option>
+    <option value="mexican">Mexican</option>
+    <option value="boba">Boba</option>
+    <option value="pizza">Pizza</option>
+    <option value="tacos">Tacos</option>
+    <option value="bbq">BBQ</option>
+    <option value="food">Food</option>
+    <option value="drinks">Drinks</option>
+    <option value="korean">Korean</option>
+    {/* <option value="chicken">Chicken</option> */}
+    <option value="coffee">Coffee</option>
+    <option value="dessert">Dessert</option>
+    <option value="pastry">Pastry</option>
+    {/* <option value="tea">Tea</option> */}
+    {/* <option value="pasta">Pasta</option> */}
+  </select>
+  {/* <input type="submit" value="Submit"></input> */}
+  <button onClick={() =>this.button(document.getElementById("terms").value)}>Submit</button>
+
+          {/* <Link to={`/businesses/search/all`} replace>
             <button onClick={() =>this.button("all")}>All</button> 
           </Link>
           <Link to={`/businesses/search/food`} replace>
@@ -100,7 +122,7 @@ class SearchIndex extends React.Component {
           </Link>
           <Link to={`/businesses/search/asian`} replace>
             <button onClick={() =>this.button("asian")}>Asian</button> 
-          </Link>                                                  
+          </Link>                                                   */}
         </div>
         <div className="bus-index-main">
           <div className="pls-search-123" >
