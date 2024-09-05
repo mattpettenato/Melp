a31f457f9d140924ca5bb05e0670da848b28ea73
mattpettenato <mattpettenato@gmail.com>
2024-09-04 23:17:37 -0700

    melp it

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -5,7 +5,7 @@ export default () => (
     <div className="hero-img-frame">
       <img className="hero-img" src="https://raw.githubusercontent.com/mattpettenato/FullStack_Project/main/melp_imgs/melp_logo.png" />
     </div>
-    <h1>Just Melp It.</h1>
+    <h1>just melp it</h1>
   </div>
 )
 
