fd53147d57c9c21fa27b2fe01ab513d27d77f81b
mattpettenato <mattpettenato@gmail.com>
2024-09-04 23:07:36 -0700

    Move listen gem to top level and adjust production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.file_watcher = ActiveSupport::FileUpdateChecker
+  # config.file_watcher = ActiveSupport::FileUpdateChecker
 end
